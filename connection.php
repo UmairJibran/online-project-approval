272057fb9f24e1ad0f602714b58946c48d23db3f
Umair Jibran <umairjibran7@gmail.com>
2020-06-22 13:13:53 +0500

    initialised Project

---
+++ connection.php
@@ -0,0 +1,13 @@
+<?php
+
+    $hostName = "localhost";
+    $userName = "root";
+    $password = "";
+    $databaseName = "propsal_management_system";
+
+   $conn = new mysqli($hostName, $userName, $password, $databaseName);
+
+    if ($conn->connect_error) {
+        die("Connection failed due to: " . $conn->connect_error);
+    }
+?>
