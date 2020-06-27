6d9a3e492675969d954590830ab1a6ca3fa32eda
Umair Jibran <umairjibran7@gmail.com>
2020-06-27 16:49:57 +0500

    Update notification_screen.php

--- student/notification_screen.php
+++ student/notification_screen.php
@@ -15,29 +15,57 @@
 </head>
 <body>
     <div class="container container-fluid">
-        <div class="jumbotron mb-5">Notifications</div>
+        <div class="jumbotron mb-5">
+            <h2 class='center'>Notifications</h2>
+        </div>
         <a href="./" class="btn btn-primary mb-3">Go Back</a>
         <?php
-            $sql = "SELECT * FROM `project_notification` WHERE `student_id` = '${stdID}'";
+            $sql = "SELECT * FROM `project_notification` WHERE `student_id` = '${stdID}' AND `seen` = 1";
             $result = $conn->query($sql);
             $rows = $result->num_rows;
             if($rows>=1){
+                echo "
+                    <table class='table'>
+                        <tr>
+                            <th>#</th>
+                            <th>Status</th>
+                            <th>Action</th>
+                        </tr>
+                ";
+                $count = 0;
                 while($data = $result->fetch_assoc()){
+                    $count+=1;
                     $prID = $data['project_id'];
                     $notID = $data['notification_id'];
-                    echo '<div class="mt-5 alert alert-warning center" role="alert">Project ';
-                    $sql1 = "SELECT `project_STATUS`  FROM `project_record` WHERE `project_ID` = `${prID}`";
+                    $sql1 = "SELECT `project_STATUS`,`project_TITLE`  FROM `project_record` WHERE `project_ID` = '${prID}'";
                     $result1 = $conn->query($sql1);
                     $data1 = $result1->fetch_assoc();
-                    if($data1 === true){
-                        echo "ACCEPTED";
+                    $title = $data1['project_TITLE'];
+                    echo "
+                        <td>${count}</td>
+                        <td><span class='mt-5 alert alert-warning center' role='alert'>`Project ${title}`
+                    ";
+                    if($data1['project_STATUS'] === true){
+                        echo " ACCEPTED";
                     }else{
-                        echo "REJECTED";
+                        echo " REJECTED";
                     }
-                    echo'</div>';
+                    echo"
+                        </span></td>
+                        <td><form method='POST'><input type='submit' value='Dissmiss' class='btn btn-info' name='seen${notID}'></form></td>
+                        </tr>
+                    ";
+                    if(isset($_POST['seen'.$notID])){
+                        $sql2 = "UPDATE `project_notification` SET `seen` = '0' WHERE `project_notification`.`notification_id` = ${notID};";
+                        $conn->query($sql2);
+                        header('location:./notification_screen.php');
+                    }
                 }
+                echo "
+                    </table>
+                ";
             }else{
-                echo '<div class="mt-5 alert alert-success center" role="alert">No Notification</div>';
+                echo '<div class="mt-5 alert alert-success center" role="alert">No New Notification</div>';
             }
         ?>
     </div>
